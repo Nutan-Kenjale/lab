23b8733970ec3dbc167e2b26d1c88bfffd489b6f
Nutan-Kenjale <nutankenjale28@gmail.com>
2021-11-09 12:19:00 +0530

    myfirst commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+       project = "teak-banner-331603"
+       region = "us-central1"
+
+}
+
+
+
+resource "google_compute_instance" "myvm" {
+    name  = "myfirstvm"
+    zone = "us-central1-a"
+    machine_type = "f1-micro"
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-10"
+
+        }
+    }
+
+     network_interface {
+         network = "default"
+         access_config{}
+     }
+}
+
+
+resource "google_compute_instance" "myvm1" {
+    name  = "myfirstvm1"
+    zone = "us-central1-a"
+    machine_type = "f1-micro"
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-10"
+
+        }
+    }
+
+     network_interface {
+         network = "default"
+         access_config{}
+     }
+}
